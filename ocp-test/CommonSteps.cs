d3c30d1a09d9b94cb13cd5c3d63ffdfd9e88ddff
dykomar <omar.ibrahim@dynamiktechnologies.com.bn>
2020-04-23 10:29:31 +0800

    Create Common Steps

---
+++ ocp-test/CommonSteps.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace ocp_test
+{
+    [Binding]
+    public sealed class CommonSteps
+    {
+        private readonly PageContext _context;
+
+        public CommonSteps(PageContext context)
+        {
+            _context = context;
+        }
+
+        [Given(@"I click the (.*) button")]
+        [When(@"I click the (.*) button")]
+        [Then(@"I click the (.*) button")]
+        public void GivenIClickThe(string buttonName)
+        {
+            //Utility.Pause(100);
+            switch (buttonName)
+            {
+                case "next":
+                    _context.CorporateRegistryServicesPage.ClickNextButton();
+                    break;
+                case "validate":
+                    _context.CorporateRegistryServicesPage.ClickValidateButton();
+                    Utility.DemoPause();
+                    break;
+                case "save":
+                    _context.CorporateRegistryServicesPage.ClickSaveButton();
+                    break;
+                case "checkbox":
+                    _context.CorporateRegistryServicesPage.ClickCheckBox();
+                    Utility.DemoPause();
+                    break;
+                case "consent":
+                    _context.CorporateRegistryServicesPage.ClickConsentButton();
+                    Utility.DemoPause();
+                    break;
+                case "register":
+                    _context.HomePage.ClickRegisterButton();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
