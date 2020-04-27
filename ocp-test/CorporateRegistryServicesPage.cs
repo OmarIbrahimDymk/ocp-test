a5af755c70f48005cbf2bd5c451ca6425842caf2
omar ibrahim <omar.ibrahim@dynamiktechnologies.com.bn>
2020-04-27 15:12:50 +0800

    CRS page - refactor

--- ocp-test/CorporateRegistryServicesPage.cs
+++ ocp-test/CorporateRegistryServicesPage.cs
@@ -9,38 +9,36 @@ namespace ocp_test
     {
         private readonly IWebDriver _driver;
         private const string PageUri = "http://localhost:8080/register";
-
-        private IWebElement _checkbox;
-        private IWebElement _saveBtn;
-        private IWebElement _nextBtn;
-        private IWebElement _checkboxWrapper;
-
-        private IWebElement _name;
-        private IWebElement _validateBtn;
-        private IWebElement _businessActivitiy;
-        private IWebElement _entitySelection;
-        private IWebElement _radioYes;
-        private IWebElement _radioGroup;
-        private IWebElement _uploadConsents;
-        private IWebElement _consentInput;
-        private IWebElement _letterInput;
-        private ReadOnlyCollection<IWebElement> _entitySelectionItems;
-
         WebDriverWait _wait;
-        private IWebElement _address1;
-        private IWebElement _addAddressBtn;
-        private ReadOnlyCollection<IWebElement> _addresses;
+
+        // CRS Page
+        private string _saveBtn = "save";
+        private string _nextBtn = "next";
+        private string _checkboxWrapper = "v-input--selection-controls";
+
+        // Important Info Tab
+        private string _checkbox = "checkbox";
+
+        // General Details Tab
+        private string _name = "name";
+        private string _validateBtn = "validate";
+        private string _businessActivitiy = "activity";
+        private string _entitySelection = "v-select__selections";
+        private string _radioYes = ".v-radio.theme--light";
+        private string _radioGroup = "[role=radiogroup]";
+        private string _uploadConsents = "v-file-input__text";
+        private string _consentInput = "consents";
+        private string _letterInput = "letter";
+
+        // Address Tab
+        private string _address1 = "address1";
+        private string _addAddressBtn = "addAddress";
 
         public CorporateRegistryServicesPage(IWebDriver driver)
         {
             _driver = driver;
 
             _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
-
-            _checkbox = _wait.Until((d) => d.FindElement(By.Id("checkbox")));
-            _saveBtn = _wait.Until((d) => d.FindElement(By.Id("save")));
-            _nextBtn = _wait.Until((d) => d.FindElement(By.Id("next")));
-            _checkboxWrapper = _wait.Until((d) => d.FindElement(By.ClassName("v-input--selection-controls")));
         }
 
         public static CorporateRegistryServicesPage NavigateTo(IWebDriver driver)
@@ -49,54 +47,33 @@ namespace ocp_test
             return new CorporateRegistryServicesPage(driver);
         }
 
-        public void InitializeGeneralDetailsElements()
-        {
-
-            _name = _wait.Until((d) => d.FindElement(By.Id("name")));
-            _validateBtn = _wait.Until((d) => d.FindElement(By.Id("validate")));
-            _businessActivitiy = _wait.Until((d) => d.FindElement(By.Id("activity")));
-            _entitySelection = _wait.Until((d) => d.FindElement(By.ClassName("v-select__selections")));
-            _radioYes = _wait.Until((d) => d.FindElement(By.CssSelector(".v-radio.theme--light")));
-            _radioGroup = _wait.Until((d) => d.FindElement(By.CssSelector("[role=radiogroup]")));
-            _uploadConsents = _wait.Until((d) => d.FindElement(By.ClassName("v-file-input__text")));
-            _consentInput = _wait.Until((d) => d.FindElement(By.Id("consents")));
-            _letterInput = _wait.Until((d) => d.FindElement(By.Id("letter")));
-        }
-
-        public void InitializeAddressElements()
-        {
-
-            _address1 = _wait.Until((d) => d.FindElement(By.Id("address1")));
-            _addAddressBtn = _wait.Until((d) => d.FindElement(By.Id("addAddress")));
-        }
-
-        public void ClickCheckBox() => _checkboxWrapper.Click();
+        public void ClickCheckBox() => _wait.Until((d) => d.FindElement(By.ClassName(_checkboxWrapper))).Click();
 
         public HomePage ClickSaveButton()
         {
-            _saveBtn.Click();
+            _wait.Until((d) => d.FindElement(By.Id(_saveBtn))).Click();
 
             return new HomePage(_driver);
         }
 
-        public void ClickNextButton() => _nextBtn.Click();
+        public void ClickNextButton() => _wait.Until((d) => d.FindElement(By.Id(_nextBtn))).Click();
 
-        public void ClickValidateButton() => _validateBtn.Click();
-        public void ClickConsent() => _uploadConsents.Click();
+        public void ClickValidateButton() => _wait.Until((d) => d.FindElement(By.Id(_validateBtn))).Click();
+        public void ClickConsent() => _wait.Until((d) => d.FindElement(By.ClassName(_uploadConsents))).Click();
 
-        public void ClickConsentButton() => _radioYes.Click();
-        public void ClickAddAddressButton() => _addAddressBtn.Click();
+        public void ClickConsentButton() => _wait.Until((d) => d.FindElement(By.CssSelector(_radioYes))).Click();
+        public void ClickAddAddressButton() => _wait.Until((d) => d.FindElement(By.Id(_addAddressBtn))).Click();
 
-        public bool IsCheckboxSelected() => _checkbox.Selected;
-        public bool IsNameVisible() => _name.Displayed;
-        public bool IsConsentRadioVisible() => _radioGroup.Displayed;
-        public bool IsUploadConsentsVisible() => _uploadConsents.Displayed;
+        public bool IsCheckboxSelected() => _wait.Until((d) => d.FindElement(By.Id(_checkbox))).Selected;
+        public bool IsNameVisible() => _wait.Until((d) => d.FindElement(By.Id(_name))).Displayed;
+        public bool IsConsentRadioVisible() => _wait.Until((d) => d.FindElement(By.CssSelector(_radioGroup))).Displayed;
+        public bool IsUploadConsentsVisible() => _wait.Until((d) => d.FindElement(By.ClassName(_uploadConsents))).Displayed;
 
-        public void SetName(string name) => _name.SendKeys(name);
-        public void SetConsents(string fileName) => _consentInput.SendKeys(fileName);
-        public void SetLetter(string fileName) => _letterInput.SendKeys(fileName);
-        public void SetActivity(string activity) => _businessActivitiy.SendKeys(activity);
-        public void SetAddress1(string activity) => _address1.SendKeys(activity);
+        public void SetName(string name) => _wait.Until((d) => d.FindElement(By.Id(_name))).SendKeys(name);
+        public void SetConsents(string fileName) => _wait.Until((d) => d.FindElement(By.Id(_consentInput))).SendKeys(fileName);
+        public void SetLetter(string fileName) => _wait.Until((d) => d.FindElement(By.Id(_letterInput))).SendKeys(fileName);
+        public void SetActivity(string activity) => _wait.Until((d) => d.FindElement(By.Id(_businessActivitiy))).SendKeys(activity);
+        public void SetAddress1(string activity) => _wait.Until((d) => d.FindElement(By.Id(_address1))).SendKeys(activity);
 
         public void SelectEntity2()
         {
@@ -108,18 +85,13 @@ namespace ocp_test
 
         public void SelectEntity()
         {
-            _entitySelection.Click();
+            _wait.Until((d) => d.FindElement(By.ClassName(_entitySelection))).Click();
 
-            _entitySelectionItems = _driver.FindElements(By.ClassName("v-list-item__title"));
+            ReadOnlyCollection<IWebElement> _entitySelectionItems = _driver.FindElements(By.ClassName("v-list-item__title"));
             Utility.DemoPause();
             _entitySelectionItems[0].Click();
         }
 
-        public int GetAddressesCount()
-        {
-            _addresses = _wait.Until((d) => d.FindElements(By.ClassName("address")));
-
-            return _addresses.Count;
-        }
+        public int GetAddressesCount() => _wait.Until((d) => d.FindElements(By.ClassName("address"))).Count;
     }
 }
